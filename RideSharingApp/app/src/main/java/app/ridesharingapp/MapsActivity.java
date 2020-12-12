9eb10c66299598029a22a8d18339a48ad9e5fd59
Radu Cotisel <raducotisel@yahoo.com>
2020-12-12 16:04:31 +0200

    Create Ride Fragment created

---
+++ RideSharingApp/app/src/main/java/app/ridesharingapp/MapsActivity.java
@@ -0,0 +1,191 @@
+package app.ridesharingapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private SupportMapFragment supportMapFragment;
+    private FusedLocationProviderClient client;
+    private GoogleMap map;
+    private FloatingActionButton searchButton;
+    private Address selectedAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.maps_dialog);
+
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        //Initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        //Check permission
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            getCurrentLocation();
+
+        } else {
+            //When permission denied, request permission
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
+        searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLocationSearch();
+            }
+        });
+
+        Button submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("location", selectedAddress);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+
+        supportMapFragment.getMapAsync(this);
+    }
+
+    private void getCurrentLocation() {
+        //Initialize task location
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                //When success
+                if (location != null) {
+                    //Sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            //Initialize latitude and  longitude
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            //Create marker options
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("Current Location");
+
+                            //Zoom map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                            //Add marker to map
+                            googleMap.addMarker(options);
+
+                            setSelectedAddress(latLng);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void onLocationSearch() {
+        Places.initialize(getApplicationContext(), getResources().getString(R.string.google_maps_key));
+
+        //Initialize fields list
+        List<Place.Field> fieldsList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME);
+        //Create intent
+        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldsList).build(MapsActivity.this);
+        //Start activity result
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //When Permission is granted
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "Cannot access location!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Place place = Autocomplete.getPlaceFromIntent(data);
+
+            //Initialize latitude and  longitude
+            LatLng latLng = place.getLatLng();
+
+            //Create marker options
+            MarkerOptions options = new MarkerOptions().position(latLng).title(place.getAddress());
+
+            //Zoom map
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+            //Add marker to map
+            map.addMarker(options);
+            setSelectedAddress(latLng);
+        }else if(resultCode == AutocompleteActivity.RESULT_ERROR){
+            Status status = Autocomplete.getStatusFromIntent(data);
+            Toast.makeText(getApplicationContext(), status.getStatusMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void setSelectedAddress(LatLng location){
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addressList = geocoder.getFromLocation(location.latitude, location.longitude, 1);
+            if(addressList!=null && !addressList.isEmpty()){
+                selectedAddress = addressList.get(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ RideSharingApp/app/src/main/java/app/ridesharingapp/UserDetailsFragment.java
@@ -0,0 +1,23 @@
+package app.ridesharingapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class UserDetailsFragment extends Fragment {
+
+    public UserDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View fragment = inflater.inflate(R.layout.fragment_user_details, container, false);
+
+        return fragment;
+    }
+}
